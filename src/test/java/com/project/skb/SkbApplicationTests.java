ca7fe8f14554fc0886dde87dd99e2d70a79c9e53
pawoo0211 <pawoo0211@naver.com>
2022-06-22 15:44:03 +0900

    "first commit"

---
+++ src/test/java/com/project/skb/SkbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.skb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
